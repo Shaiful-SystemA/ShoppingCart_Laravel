c38453caed7fdc44c7972452919b7c37c10d51a6
shaiful_systema <e0001339@u.nus.edu>
2016-12-06 09:48:17 +0000

    Before break

--- resources/views/Shop/index.blade.php
+++ resources/views/Shop/index.blade.php
@@ -14,17 +14,113 @@
       <img src="http://childrensbookalmanac.com/wp-content/uploads/Harry-Potter.png" alt="..." class="img-responsive">
       <div class="caption">
         <h3>Product Titile</h3>
-        <p>
+        <p class="description">
         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.    
             
         </p>
         <div class = "clearfix" >
-            <a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default pull right" role="button">Button</a>
+            <div class="pull-left price">$12 </div>
+            <a href="#" class="btn btn-success pull-right" role="button">Button</a>
          </div>
       </div>
     </div>
   </div>
+  
+  
+    <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="http://childrensbookalmanac.com/wp-content/uploads/Harry-Potter.png" alt="..." class="img-responsive">
+      <div class="caption">
+        <h3>Product Titile</h3>
+        <p class="description">
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.    
+            
+        </p>
+        <div class = "clearfix" >
+            <div class="pull-left price">$12 </div>
+            <a href="#" class="btn btn-success pull-right" role="button">Button</a>
+         </div>
+      </div>
+    </div>
+  </div>
+  
+  
+  
+    <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="http://childrensbookalmanac.com/wp-content/uploads/Harry-Potter.png" alt="..." class="img-responsive">
+      <div class="caption">
+        <h3>Product Titile</h3>
+        <p class="description">
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.    
+            
+        </p>
+        <div class = "clearfix" >
+            <div class="pull-left price">$12 </div>
+            <a href="#" class="btn btn-success pull-right" role="button">Button</a>
+         </div>
+      </div>
+    </div>
+  </div>
+  
 </div>
 
 
-@endsection
+
+<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="http://childrensbookalmanac.com/wp-content/uploads/Harry-Potter.png" alt="..." class="img-responsive">
+      <div class="caption">
+        <h3>Product Titile</h3>
+        <p class="description">
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.    
+            
+        </p>
+        <div class = "clearfix" >
+            <div class="pull-left price">$12 </div>
+            <a href="#" class="btn btn-success pull-right" role="button">Button</a>
+         </div>
+      </div>
+    </div>
+  </div>
+  
+  
+    <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="http://childrensbookalmanac.com/wp-content/uploads/Harry-Potter.png" alt="..." class="img-responsive">
+      <div class="caption">
+        <h3>Product Titile</h3>
+        <p class="description">
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.    
+            
+        </p>
+        <div class = "clearfix" >
+            <div class="pull-left price">$12 </div>
+            <a href="#" class="btn btn-success pull-right" role="button">Button</a>
+         </div>
+      </div>
+    </div>
+  </div>
+  
+  
+  
+    <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="http://childrensbookalmanac.com/wp-content/uploads/Harry-Potter.png" alt="..." class="img-responsive">
+      <div class="caption">
+        <h3>Product Titile</h3>
+        <p class="description">
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.    
+            
+        </p>
+        <div class = "clearfix" >
+            <div class="pull-left price">$12 </div>
+            <a href="#" class="btn btn-success pull-right" role="button">Button</a>
+         </div>
+      </div>
+    </div>
+  </div>
+  
+</div>
+
