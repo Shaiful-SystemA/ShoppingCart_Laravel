55549de94b2eefd6bb56063ab3367e46385d4ed6
shaiful_systema <e0001339@u.nus.edu>
2016-12-07 08:02:04 +0000

    before launch

--- resources/views/Partials/header.blade.php
+++ resources/views/Partials/header.blade.php
@@ -20,7 +20,7 @@
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user" aria-hidden="true"></i>User Management <span class="caret"></span></a>
           <ul class="dropdown-menu">
             <li><a href=" {{route('User.signup')  }} ">Signup</a></li>
-            <li><a href="#">Signin</a></li>
+            <li><a href=" {{route('User.signin')  }} ">Signup</a></li>
            <li role="separator" class="divider"></li>
             <li><a href="#">Separated link</a></li>
           </ul>
